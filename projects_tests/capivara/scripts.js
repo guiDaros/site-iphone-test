9119855780b6107970711f4a073c7895efc34f57
guiDaros <guivdaros@gmail.com>
2023-04-15 18:43:35 -0300

    Page to link my instagram link and my insta Qr together.

---
+++ projects_tests/capivara/scripts.js
@@ -0,0 +1,15 @@
+function trade(image){
+    let insta = document.querySelector(".qr")
+    insta.src = image
+}
+
+function tradeText(){
+    document.getElementById("qr-text").innerHTML = "Scan the QR code to visit my Instagram page."
+}
+
+function tradeText2(){
+    document.getElementById("qr-text").innerHTML = "Follow the link to visit my Instagram page."
+}
+
+
+
